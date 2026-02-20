a9bda5555338ba08a8aa88ae73b4582bb699e4e4
soporteremoto2016 <soporteremoto2016@gmail.com>
2026-02-19 20:40:03 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
 st.title("Mi Primera pagina Iteractiva")
+# Texto
+st.write("Esta es una interfaz muy sencilla.")
+
+# Entrada de texto
+nombre = st.text_input("Escribe tu nombre:")
+
+# Botón
+if st.button("Saludar"):
+    st.success(f"Hola {nombre}, bienvenido a Streamlit 🚀")
+
+# Slider
+edad = st.slider("Selecciona tu edad:", 0, 100, 25)
+st.write("Tu edad es:", edad)
